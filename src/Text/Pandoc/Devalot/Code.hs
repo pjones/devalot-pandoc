394a04c680dbb83c9c55b44490281df3bca107c9
Peter Jones <pjones@pmade.com>
2013-09-23 12:06:50 -0600

    Add support for narrowing code to token delimiters

--- src/Text/Pandoc/Devalot/Code.hs
+++ src/Text/Pandoc/Devalot/Code.hs
@@ -16,6 +16,11 @@ module Text.Pandoc.Devalot.Code
        ) where
 
 --------------------------------------------------------------------------------
+import Control.Monad ((<=<))
+import Data.Monoid
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
 import Text.Pandoc
 
 --------------------------------------------------------------------------------
@@ -27,10 +32,55 @@ import Text.Pandoc
 -- > this will be replaced by contents of README
 -- > ~~~~
 --
+-- The file can be narrowed to the text between delimiters.  The
+-- delimiters are @{BEGIN: token}@ and @{END}@ and can be used like
+-- this:
+--
+-- > ~~~~ {include="README" token="foo"}
+-- > ~~~~
+--
 -- Taken from <http://johnmacfarlane.net/pandoc/scripting.html#include-files>.
 includeFile :: Block -> IO Block
 includeFile cb@(CodeBlock (x, y, alist) _) =
   case lookup "include" alist of
-       Just f  -> return . (CodeBlock (x, y, alist)) =<< readFile f
+       Just f  -> return . (CodeBlock (x, y, alist)) =<< newtxt f
        Nothing -> return cb
+  where newtxt f = readCodeFile f (lookup "token" alist)
 includeFile x = return x
+
+--------------------------------------------------------------------------------
+-- | Read a file with code in it, possibly narrowing to a token.
+readCodeFile :: FilePath -> Maybe String -> IO String
+readCodeFile path Nothing      = readFile path
+readCodeFile path (Just token) = do
+  contents <- T.readFile path
+  case narrowToToken token contents of
+    Nothing  -> error $ "can't find token '" ++ token ++ "' in " ++ path
+    Just txt -> return $! T.unpack txt
+
+--------------------------------------------------------------------------------
+-- | Narrow the given text to a beginning and ending delimiter.
+narrowToToken :: String -> Text -> Maybe Text
+narrowToToken token = matchEnd <=< matchStart
+  where
+    start :: Text
+    start = "{BEGIN: " <> T.pack token <> "}"
+
+    end :: Text
+    end = "{END}"
+
+    matchStart :: Text -> Maybe Text
+    matchStart txt = case T.breakOn start txt of
+      (_, match) | T.null match -> Nothing
+                 | otherwise    -> Just $! stripStart match
+
+    matchEnd :: Text -> Maybe Text
+    matchEnd txt = case T.breakOn end txt of
+      (prefix, match) | T.null match -> Nothing
+                      | otherwise    -> Just $! stripEnd prefix
+
+    stripStart :: Text -> Text
+    stripStart = T.stripStart . T.drop (T.length start)
+
+    stripEnd :: Text -> Text
+    stripEnd = T.dropWhileEnd (/= '\n')

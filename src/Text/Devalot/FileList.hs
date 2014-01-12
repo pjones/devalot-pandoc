c0615a5a6e714f0aab7515954ec36469f047fee6
Peter Jones <pjones@pmade.com>
2014-01-12 11:50:00 -0700

    New FileList parser that can handle comments anywhere, even just before EOF

--- src/Text/Devalot/FileList.hs
+++ src/Text/Devalot/FileList.hs
@@ -10,10 +10,12 @@ terms contained in the LICENSE file.
 -}
 
 --------------------------------------------------------------------------------
+-- | Parse a file that lists file names and return the file names.
+-- Supports Unix-style comments.
 module Text.Devalot.FileList (FileList, files, parseFile) where
 
 --------------------------------------------------------------------------------
-import Control.Applicative hiding ((<|>))
+import Control.Applicative hiding ((<|>), many)
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
@@ -25,6 +27,22 @@ import Text.Parsec.Text
 newtype FileList = FileList
   { files :: [Text] -- ^ List of file names.
   } deriving Show
+
+--------------------------------------------------------------------------------
+-- | Tokens that can be parsed.
+data Token = Comment | FileName Text
+
+--------------------------------------------------------------------------------
+-- | Extracts file names from a list of tokens.
+onlyFiles :: [Token] -> [Text]
+onlyFiles = concatMap extract . filter predicate where
+  predicate :: Token -> Bool
+  predicate Comment      = False
+  predicate (FileName _) = True
+
+  extract :: Token -> [Text]
+  extract Comment      = []
+  extract (FileName x) = [x]
 
 --------------------------------------------------------------------------------
 -- | Parse a file and return a 'FileList'.
@@ -41,16 +59,26 @@ parseFile file = (convert . parse) <$> T.readFile file where
 --------------------------------------------------------------------------------
 -- | Parses a list of file names.
 fileList :: Parser FileList
-fileList = FileList <$> manyTill (fileName <* spaces) eof
+fileList = FileList . onlyFiles <$>
+           manyTill (whitespace <|> comment <|> fileName) eof
 
 --------------------------------------------------------------------------------
 -- | Parse a file name.
-fileName :: Parser Text
-fileName = do
-  comment <|> spaces
-  T.pack <$> manyTill anyChar (newline <|> space) <?> "file name"
+fileName :: Parser Token
+fileName = do first  <- anyChar
+              others <- manyTill anyChar endFileName
+              return . FileName . T.strip . T.pack $ (first : others)
+           <?> "file name"
+  where
+    endFileName :: Parser Char
+    endFileName = lookAhead (newline <|> char '#')
 
 --------------------------------------------------------------------------------
--- | Parse a comment.
-comment :: Parser ()
-comment = spaces >> char '#' >> (manyTill anyChar newline <?> "comment") >> spaces
+-- | Parse out whitespace as a comment.
+whitespace :: Parser Token
+whitespace = many1 space >> return Comment
+
+--------------------------------------------------------------------------------
+-- | Parse comments.
+comment :: Parser Token
+comment = char '#' >> manyTill anyChar newline >> return Comment <?> "comment"
